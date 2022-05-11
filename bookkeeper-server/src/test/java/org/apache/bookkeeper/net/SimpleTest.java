765bacb430a819906d79e5bf84cb725d39cc1e92
cristiano-u08 <cristia_no98@live.it>
2022-05-11 16:41:50 +0200

    Fixed pom and added dummy test for JaCoCo coverage calculation

---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/net/SimpleTest.java
@@ -0,0 +1,11 @@
+package org.apache.bookkeeper.net;
+
+import org.junit.Test;
+
+public class SimpleTest {
+
+	@Test
+	public void simpleMethodTest() {
+		NetworkTopology t = new NetworkTopologyImpl();
+	}
+}
